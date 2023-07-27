06eec0b5a1c72d1ceb98c9e39ece008cd405f6f6
qvixnh <nguyenvinh22012002@gmail.com>
2023-07-27 16:17:25 +0700

    Delegate with external class

--- DelegateBasicExample/Program.cs
+++ DelegateBasicExample/Program.cs
@@ -9,23 +9,28 @@ namespace DelegateBasicExample
         delegate void LogDel(string text);
         static void Main(string[] args)
         {
+            Log log = new Log();
             //LogDel logDel = new LogDel(LogTextToScreen);
-            LogDel logDel = new LogDel(LogTextToFile);
+            LogDel logDel = new LogDel(log.LogTextToScreen);
 
             Console.WriteLine("Please enter your name: ");
             var name = Console.ReadLine();
             logDel(name);
             Console.ReadKey();
         }
-        static void LogTextToScreen(string text)
+        
+    }
+    public class Log
+    {
+        public void LogTextToScreen(string text)
         {
             Console.WriteLine($"{DateTime.Now}: {text}");
         }
         //wrties the user input to a text file
-        static void LogTextToFile(string text)
+        public void LogTextToFile(string text)
         {
             //logic code
-            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"),true))
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
             {
                 sw.WriteLine($"{DateTime.Now}: {text}");
             }
